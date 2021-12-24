df7db5a0a0ba29b2b1ba88904978fb8db5bef0b9
XMoSalahX <01154615199>
2021-12-24 20:52:03 +0200

    New

--- sw.js
+++ sw.js
@@ -84,12 +84,19 @@ self.addEventListener('fetch', (e) => {
         if (r) {
             return r;
         }
+        fetch(e.request)
+            .then(function(response) {
 
-        const response = await fetch(e.request);
-        const cache = await caches.open(cacheName);
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-        cache.put(e.request, response.clone());
-        return response;
+                return caches.open(cacheName).then(function(cache) {
+
+                    cache.put(e.request.url, response.clone());
+
+                    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+                    return response;
+                });
+            })
+
+
 
 
     })());
